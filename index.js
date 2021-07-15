77c40ecf94da03ac734a4d7fcffeafdca1cd7c48
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-15 16:05:03 -0300

    auth middleware

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app = express();
 
 app.use(express.json());
 
+let auth = function(req, res, next) {
+    let value = 'Bearer 65a83e72c7e990a3e6565ae8b7cc071c';
+    let token = req.get('authorization');
 
+    console.log(token);
+    if(token === value){
+        next();
+    }else{
+        res.status(401).send('Unauthorized')
+    }
+}
 
-app.get('/', function(req, res){
+app.get('/', auth, function(req, res){
     res.sendFile(__dirname + '/src/views/home.html')
   })
 
